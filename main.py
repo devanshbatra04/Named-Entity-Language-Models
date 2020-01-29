d9641764e7a89f99da23770562ba834120fb5628
Devansh Batra <devanshbatra04@gmail.com>
2020-01-29 23:12:37 +0530

    added batches for dataset without types

--- main.py
+++ main.py
@@ -21,3 +21,7 @@ if __name__ == "__main__":
     train_data_with_types = batchify(corpus_with_types.train, BATCH_SIZE, args)
     val_data_with_types = batchify(corpus_with_types.valid, BATCH_SIZE, args)
     test_data_with_types = batchify(corpus_with_types.test, BATCH_SIZE, args)
+
+    train_data_without_types = batchify(corpus_without_types.train, BATCH_SIZE, args)
+    val_data_without_types = batchify(corpus_without_types.valid, BATCH_SIZE, args)
+    test_data_without_types = batchify(corpus_without_types.test, BATCH_SIZE, args)
