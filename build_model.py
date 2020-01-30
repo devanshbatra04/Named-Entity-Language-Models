7660e561501e40bbea45263761b8fd424e89a8d2
Devansh Batra <devanshbatra04@gmail.com>
2020-01-30 16:34:27 +0530

    added eval function

--- build_model.py
+++ build_model.py
@@ -2,6 +2,8 @@ AWDM_LSTM_PATH = "./awd-lstm-lm/"
 import sys
 sys.path.insert(0, AWDM_LSTM_PATH)
 from awd_lstm.model import RNNModel
+from awd_lstm.utils import get_batch, repackage_hidden
+
 ###############################################################################
 # Build the models
 ###############################################################################
@@ -33,3 +35,18 @@ def get_model(model_type, corpus, em_size, n_hid, n_layers, args):
     params = list(model.parameters()) + list(criterion.parameters())
 
     return model, criterion, params
+
+
+def evaluate(model, criterion, model_type, data_source, args, batch_size=10):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    if model_type == 'QRNN':
+        model.reset()
+    total_loss = 0
+    hidden = model.init_hidden(batch_size)
+    for i in range(0, data_source.size(0) - 1, args.bptt):
+        data, targets = get_batch(data_source, i, args, evaluation=True)
+        output, hidden = model(data, hidden)
+        total_loss += len(data) * criterion(model.decoder.weight, model.decoder.bias, output, targets).data
+        hidden = repackage_hidden(hidden)
+    return total_loss.item() / len(data_source)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, AWDM_LSTM_PATH)
 import argparse
 from data_utils import load_datasets
-from awd_lstm.utils import batchify, get_batch
+from awd_lstm.utils import batchify
 from build_model import get_model
 
 DATA_WITH_TYPES = "./data_with_type"
@@ -46,5 +46,5 @@ if __name__ == "__main__":
     val_data_without_types = batchify(corpus_without_types.valid, BATCH_SIZE, args)
     test_data_without_types = batchify(corpus_without_types.test, BATCH_SIZE, args)
 
-    data_model_with_type = get_model(MODEL_TYPE, corpus_with_types, EMBEDDING_SIZE, NUM_HIDDEN_UNITS_PER_LAYER, NUM_LAYERS, args)
-    data_model_without_type = get_model(MODEL_TYPE, corpus_with_types, EMBEDDING_SIZE, NUM_HIDDEN_UNITS_PER_LAYER, NUM_LAYERS, args)
+    model_with_type, criterion_model_with_type, params_model_with_type,  = get_model(MODEL_TYPE, corpus_with_types, EMBEDDING_SIZE, NUM_HIDDEN_UNITS_PER_LAYER, NUM_LAYERS, args)
+    model_without_type, criterion_model_without_type, params_model_without_type,  = get_model(MODEL_TYPE, corpus_with_types, EMBEDDING_SIZE, NUM_HIDDEN_UNITS_PER_LAYER, NUM_LAYERS, args)
